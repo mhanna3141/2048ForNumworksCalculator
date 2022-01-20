f6999536b9e6ff24942c2d2468a71369940cfafe
Mark Hanna <glorifedstoplight@gmail.com>
2022-01-19 19:26:04 -0500

    Finished the function that shifts everything a specified direction

--- TheGame.py
+++ TheGame.py
@@ -3,41 +3,98 @@
 def shiftListRight(givenList):
 
     # loop through list
-    for i in range(2):
-        for itemIndex in range(len(givenList)):
+    for itemIndex in range(len(givenList)):
 
-            # item is None or no where to move item
-            if givenList[itemIndex] is None or itemIndex == gameGridDim-1:
-                continue
+        # item is None or no where to move item
+        if givenList[itemIndex] is None or itemIndex == gameGridDim-1:
+            continue
 
-            else:
+        else:
 
-                # can combined current index and next index
-                if givenList[itemIndex] == givenList[itemIndex+1]:
-                    givenList[itemIndex + 1] = givenList[itemIndex] * 2
-                    del givenList[itemIndex]
-                    givenList.insert(0, None)
+            # can combined current index and next index
+            if givenList[itemIndex] == givenList[itemIndex+1]:
+                givenList[itemIndex + 1] = givenList[itemIndex] * 2
+                del givenList[itemIndex]
+                givenList.insert(0, None)
 
-                # just move current index to the right
-                elif givenList[itemIndex + 1] is None:
-                    del givenList[itemIndex + 1]
-                    givenList.insert(0, None)
+            # just move current index to the right
+            elif givenList[itemIndex + 1] is None:
+                del givenList[itemIndex + 1]
+                givenList.insert(0, None)
 
     return givenList
 
 
 #
-def shiftGameGrid(xDirection, yDirection):
-    pass
+def shiftGameGrid(direction):
+
+    # merge right
+    if direction == "RIGHT":
+
+        # loop through rows
+        for rowIndex in range(len(gameGrid)):
+
+            gameGrid[rowIndex] = shiftListRight(gameGrid[rowIndex])
+
+    # merge left
+    elif direction == "LEFT":
+
+        # loop through rows
+        for rowIndex in range(len(gameGrid)):
+            sendList = gameGrid[rowIndex]
+            sendList.reverse()
+            sendList = shiftListRight(sendList)
+            sendList.reverse()
+            gameGrid[rowIndex] = sendList
+
+    elif direction == "DOWN":
+
+        for columnIndex in range(len(gameGrid[0])):
+
+            sendList = []
+
+            # loop through rows
+            for row in gameGrid:
+
+                sendList.append(row[columnIndex])
+
+            sendList = shiftListRight(sendList)
+
+            for rowIndex in range(len(sendList)):
+                gameGrid[rowIndex][columnIndex] = sendList[rowIndex]
+
+    else:
+
+        for columnIndex in range(len(gameGrid[0])):
+
+            sendList = []
+
+            # loop through rows
+            for row in gameGrid:
+                sendList.append(row[columnIndex])
+
+            sendList.reverse()
+            sendList = shiftListRight(sendList)
+            sendList.reverse()
+
+            for rowIndex in range(len(sendList)):
+                gameGrid[rowIndex][columnIndex] = sendList[rowIndex]
 
 
 gameGridDim = 4
 
 gameGrid = [
-    [None, None, None, None],
-    [None, None, None, None],
-    [None, None, None, None],
-    [None, None, None, None]
+    [None, None, 2, 2],
+    [2, None, None, None],
+    [4, 4, 8, 4],
+    [16, None, 2, None]
 ]
 
 print(shiftListRight([2, 2, 2, 2]))
+print()
+for row in gameGrid:
+    print(row)
+shiftGameGrid("UP")
+print()
+for row in gameGrid:
+    print(row)
